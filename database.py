d6f361e3945494e1c3c45c6b3bd5d9f9bc9aa21d
merrittzhang <zhangmerritt@gmail.com>
2023-12-13 22:53:58 -0500

    backend functions

--- database.py
+++ database.py
@@ -69,22 +69,52 @@ def get_all(db_file, table):
         
 """
 updates rows in table from db_file. In row with primary key 
-unique_id, update the columns indicated by all the keys in 
+classid, update the columns indicated by all the keys in 
 dictionary values with all the values in dictionary values
 """
-def update(db_file, table, values, unique_id)
+def update(db_file, table, values, classid):
+    check_table(db_file, table)
+    set_clause = ", ".join([f"{key} = ?" for key in values.keys()])
+    parameters = list(values.values()) + [classid]
+
+    with get_connection(db_file) as conn:
+        with contextlib.closing(conn.cursor()) as cursor:
+            sql_query = f"UPDATE {table} SET {set_clause} WHERE classid = ?"
+            cursor.execute(sql_query, parameters)
+            conn.commit()
+
+    
 
 """
 add a row to table from db_file. Add values from dictionary
 values to columns indicated by keys in dictionary values
 """
-def insert(db_file, table, values)
+def insert(db_file, table, values):
+    check_table(db_file, table)
+    columns = ', '.join(values.keys())
+    placeholders = ', '.join(['?'] * len(values))
+    parameters = list(values.values())
+
+    with get_connection(db_file) as conn:
+        with contextlib.closing(conn.cursor()) as cursor:
+            sql_query = f"INSERT INTO {table} ({columns}) VALUES ({placeholders})"
+            cursor.execute(sql_query, parameters)
+            conn.commit()
+
     
 """
-deletes a row with primary key unique_id from table 
+deletes a row with primary key classid from table 
 in db_file.
 """
-def delete(db_file, table, unique_id)
+def delete(db_file, table, classid):
+    check_table(db_file, table)
+
+    with get_connection(db_file) as conn:
+        with contextlib.closing(conn.cursor()) as cursor:
+            sql_query = f"DELETE FROM {table} WHERE classid = ?"
+            cursor.execute(sql_query, (classid,))
+            conn.commit()
+
 
 def test():
     db_file = "reg.sqlite"
@@ -98,6 +128,45 @@ def test():
     print(f"Num classes: {len(classes)}")
     print(f"First 10:")
     for clss in classes[0:10]:
+        print(f"{clss['classid']} - {clss['courseid']}")
+
+    delete(db_file, 'classes', 7838)
+
+    classes = get_all(db_file, 'classes')
+    print(f"Num classes: {len(classes)}")
+    print(f"First 10:")
+    for clss in classes[0:10]:
+        print(f"{clss['classid']} - {clss['courseid']}")
+
+
+    class_dict = {
+        'classid' : 7838, 
+        'courseid': 3457, 
+        'days': 'M',
+        'starttime': '01:30 PM',
+        'endtime': '04:20 PM', 
+        'bldg': 'CHANC', 
+        'roomnum': 105,
+    }
+
+    insert(db_file, 'classes', class_dict)
+
+    classes = get_all(db_file, 'classes')
+    print(f"Num classes: {len(classes)}")
+    print(f"First 10:")
+    for clss in classes[0:len(classes)]:
+        print(f"{clss['classid']} - {clss['courseid']}")
+
+    update_dict = {
+        'courseid': 3458, 
+    }
+
+    update(db_file, 'classes', update_dict, 7838)
+
+    classes = get_all(db_file, 'classes')
+    print(f"Num classes: {len(classes)}")
+    print(f"First 10:")
+    for clss in classes[0:len(classes)]:
         print(f"{clss['classid']} - {clss['courseid']}")
 
 
