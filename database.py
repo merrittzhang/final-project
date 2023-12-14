ccd6567e543c98f7fbe0a609e19601136580b83a
Samuel Sanft <sanft@princeton.edu>
2023-12-13 20:39:11 -0500

    basic database functionality

--- database.py
+++ database.py
@@ -1,127 +1,79 @@
-"""
-database.py
-Authors: Merritt Zhang, Sam Sanft
-"""
-
 import contextlib
 import sqlite3
 
 
-DATABASE_URL = 'file:reg.sqlite?mode=ro'
+class DatabaseError(Exception):
+    def __init__(self, string):
+        self.error = string
+    
+    def __str__(self):
+        return self.error
 
 
-def get_classes(dept="", coursenum="", area="", title=""):
-    """
-    Returns a list of dictionary objects that correspond to classes.
-    Returns all classes by default, keyword args can be used to specify
-    which classes to return.
-    """
-    with sqlite3.connect(DATABASE_URL, isolation_level=None,
-                         uri=True) as connection:
-        with contextlib.closing(connection.cursor()) as cursor:
-            courses = []
-            # Get data from database
-            stmt = "SELECT classid, classes.courseid, dept, " \
-                   "coursenum, area, title "
-            stmt += "FROM classes, courses, crosslistings "
-            stmt += "WHERE classes.courseid = courses.courseid "
-            stmt += "AND classes.courseid = crosslistings.courseid "
-            args = []
-
-            # Check flags
-            if dept:
-                dept = dept.replace("%", "\\%").replace("_", "\\_")
-                stmt += 'AND dept LIKE ? '
-                args.append("%" + dept + "%")
-            if coursenum:
-                num = coursenum.replace("%", "\\%").replace("_", "\\_")
-                stmt += 'AND coursenum LIKE ? '
-                args.append("%" + num + "%")
-            if area:
-                area = area.replace("%", "\\%").replace("_", "\\_")
-                stmt += 'AND area LIKE ? '
-                args.append("%" + area + "%")
-            if title:
-                title = title.replace("%", "\\%").replace("_", "\\_")
-                stmt += 'AND title LIKE ? '
-                args.append("%" + title + "%")
-
-            if len(args) > 0:
-                stmt += 'ESCAPE "\\" '
-
-            stmt += "ORDER BY dept, coursenum "
-
-            # Return result
-            cursor.execute(stmt, args)
-            for entry in cursor.fetchall():
-                courses.append({"classid": entry[0],
-                                "courseid": entry[1],
-                                "dept": entry[2],
-                                "coursenum": entry[3],
-                                "area": entry[4],
-                                "title": entry[5]})
-            return courses
+def get_connection(db_file):
+    conn = sqlite3.connect(db_file)
+    return conn
 
 
-def get_class_info(classid):
-    """
-    Returns a dictionary object that correspond to class info for
-    classid.
-    """
-    with sqlite3.connect(DATABASE_URL, isolation_level=None,
-                         uri=True) as connection:
-        with contextlib.closing(connection.cursor()) as cursor:
-            info = {}
+def check_table(db_file, table):
+    tables = get_table_names(db_file)
+    if table not in tables:
+        raise DatabaseError(f"Table {table} does not exist in {db_file}")
 
-            # Get data from classes
-            stmt = "SELECT courseid, days, starttime, endtime, bldg, " \
-                   "roomnum "
-            stmt += "FROM classes WHERE classid = ?"
-            cursor.execute(stmt, [classid])
-            result = cursor.fetchall()
+def get_table_names(db_file):
+    with get_connection(db_file) as conn:
+        with contextlib.closing(conn.cursor()) as cursor:
+            sql_query = "SELECT name FROM sqlite_master WHERE type='table'"
+            cursor.execute(sql_query)
+            rows = cursor.fetchall()
+            tables = [row[0] for row in rows]
+            return tables
+        
 
-            if len(result) == 0:
-                return None
-            courseid = result[0][0]
-            info.update({"courseid": result[0][0],
-                         "days": result[0][1],
-                         "starttime": result[0][2],
-                         "endtime": result[0][3],
-                         "bldg": result[0][4],
-                         "roomnum": result[0][5]})
+def get_columns(db_file, table):
+    check_table(db_file, table)
+    
+    with get_connection(db_file) as conn:
+        with contextlib.closing(conn.cursor()) as cursor:
+            sql_query = f"SELECT * FROM {table}" 
+            cursor.execute(sql_query)
+            columns = [col[0] for col in cursor.description]
+            return columns
+        
 
-            # Get data from courses
-            stmt = "SELECT area, title, descrip, prereqs "
-            stmt += f"FROM courses WHERE courseid = {courseid}"
-            cursor.execute(stmt)
-            result = cursor.fetchall()
-            info.update({"area": result[0][0],
-                         "title": result[0][1],
-                         "descrip": result[0][2],
-                         "prereqs": result[0][3]})
+def get_all(db_file, table):
+    check_table(db_file, table)
 
-            # Get data from crosslistings
-            depts = []
-            nums = []
-            stmt = "SELECT dept, coursenum "
-            stmt += f"FROM crosslistings WHERE courseid = {courseid} "
-            stmt += "ORDER BY dept, coursenum"
-            cursor.execute(stmt)
-            result = cursor.fetchall()
-            for entry in result:
-                depts.append(entry[0])
-                nums.append(entry[1])
-            info.update({"dept": depts, "coursenum": nums})
+    columns = get_columns(db_file, table)
 
-            # Get data from coursesprofs
-            stmt = "SELECT profname "
-            stmt += "FROM coursesprofs, profs "
-            stmt += f"WHERE courseid = {courseid} "
-            stmt += "AND coursesprofs.profid = profs.profid "
-            stmt += "ORDER BY profname"
-            cursor.execute(stmt)
-            result = cursor.fetchall()
-            profs = [entry[0] for entry in result]
-            info.update({"prof": profs})
+    data = []
 
-            return info
+    with get_connection(db_file) as conn:
+        with contextlib.closing(conn.cursor()) as cursor:
+            sql_query = f"SELECT * FROM {table}"
+            cursor.execute(sql_query)
+            for row in cursor.fetchall():
+                entry = {}
+                for i, column in enumerate(columns):
+                    entry[column] = row[i]
+                data.append(entry)
+            return data
+    
+
+def test():
+    db_file = "reg.sqlite"
+    tables = get_table_names(db_file)
+    print(tables)
+
+    columns = get_columns(db_file, 'classes')
+    print(columns)
+
+    classes = get_all(db_file, 'classes')
+    print(f"Num classes: {len(classes)}")
+    print(f"First 10:")
+    for clss in classes[0:10]:
+        print(f"{clss['classid']} - {clss['courseid']}")
+
+
+if __name__ == "__main__":
+    test()
