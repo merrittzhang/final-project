6d9159b682b9100454448f4881dc5e095e21ebc2
Samuel Sanft <sanft@princeton.edu>
2023-12-15 13:21:41 -0500

    edit

--- static/scripts/table.js
+++ static/scripts/table.js
@@ -14,8 +14,8 @@ async function tableSetup() {
 
     let tableContainer = $('<div/>', {class: "flex-grow-1 d-flex overflow-auto border border-primary rounded-3"}).appendTo($('#bodyDiv'))
 
-    let table = $('<table/>', {id: "mainTable", class: "table table-striped"}).appendTo(tableContainer);
-    fillTable(table, columns, data);
+    $('<table/>', {id: "mainTable", class: "table table-striped"}).appendTo(tableContainer);
+    fillTable(columns, types, data);
 
     let addButton = $('<button/>', {id: "addButton", class: "btn btn-success"}).text("Insert Row").appendTo($('#bodyDiv'));
 
@@ -26,9 +26,9 @@ async function tableSetup() {
     })
 }
 
-function fillTable(table, columns, data) {
-    table.empty();
-    let head = $('<thead/>').appendTo($(table));
+function fillTable(columns, types, data) {
+    $('#mainTable').empty();
+    let head = $('<thead/>').appendTo($('#mainTable'));
     head = $('<tr/>').appendTo(head);
 
     $('<th/>').text("Modify").appendTo(head);
@@ -37,48 +37,55 @@ function fillTable(table, columns, data) {
         $('<th/>').text(col).appendTo(head);
     }
 
-    let body = $('<tbody/>', {id: "tableBody"}).appendTo($(table));
+    let body = $('<tbody/>', {id: "tableBody"}).appendTo($('#mainTable'));
 
     for (const row of data) {
         let tableRow = $('<tr />').appendTo(body);
-
-        let buttonsCell = $('<th/>').appendTo(tableRow);
-        let buttonsDiv = $('<div/>', {class: "d-flex"}).appendTo(buttonsCell);
-        let editButton = $('<button/>', {class: "btn btn-circle-sm btn-warning"}).text("Edit").appendTo(buttonsDiv);
-        let deleteButton = $('<button/>', {class: "btn btn-circle-sm btn-danger"}).text("Delete").appendTo(buttonsDiv);
-
-        for (const col of columns) {
-            $('<th/>').text(row[col]).appendTo(tableRow);
-        }
-
-        deleteButton.click(() => {
-            let identifiers = row;
-            console.log(row);
-            let requestData = {
-                type: 'POST',
-                url: "/api/delete/" + activeTable,
-                data: JSON.stringify(identifiers),
-                contentType: 'application/json',
-                success: async function () {
-                    console.log("Successfully received data");
-                    var request = await fetch("/api/get_all/" + activeTable);
-                    const response = await request.json();
-
-                    let newColumns = response["columns"];
-                    let newData = response["data"];
-                    fillTable(table, newColumns, newData);
-                    $('#addButton').show();
-                    return;
-                },
-                error: function () {
-                    console.log("Request failed");
-                    alert("Request failed");
-                    return;
-                }
-            };
-            $.ajax(requestData);
-        })
+        makeRow(columns, types, tableRow, row);        
     }
+}
+
+function makeRow(columns, types, tableRow, row) {
+    let buttonsCell = $('<th/>').appendTo(tableRow);
+    let buttonsDiv = $('<div/>', {class: "d-flex"}).appendTo(buttonsCell);
+    let editButton = $('<button/>', {class: "btn btn-circle-sm btn-warning"}).text("Edit").appendTo(buttonsDiv);
+    let deleteButton = $('<button/>', {class: "btn btn-circle-sm btn-danger"}).text("Delete").appendTo(buttonsDiv);
+
+    for (const col of columns) {
+        $('<th/>').text(row[col]).appendTo(tableRow);
+    }
+
+    editButton.click(() => {
+        editRow(columns, types, tableRow, row);
+    })
+
+    deleteButton.click(() => {
+        let identifiers = row;
+        console.log(row);
+        let requestData = {
+            type: 'POST',
+            url: "/api/delete/" + activeTable,
+            data: JSON.stringify(identifiers),
+            contentType: 'application/json',
+            success: async function () {
+                console.log("Successfully received data");
+                var request = await fetch("/api/get_all/" + activeTable);
+                const response = await request.json();
+
+                let newColumns = response["columns"];
+                let newData = response["data"];
+                fillTable(newColumns, types, newData);
+                $('#addButton').show();
+                return;
+            },
+            error: function () {
+                console.log("Request failed");
+                alert("Request failed");
+                return;
+            }
+        };
+        $.ajax(requestData);
+    })
 }
 
 function insertRow(columns, types) {
@@ -90,62 +97,96 @@ function insertRow(columns, types) {
 
     for (const col of columns) {
         let cell = $('<th/>').appendTo(tableRow);
-        $('<input/>', {id: "input" + col, type: "text", class: "form-control", placeholder: col}).appendTo(cell);
+        $('<input/>', {id: "input_insert" + col, type: "text", class: "form-control", placeholder: col}).appendTo(cell);
     }
 
     submitButton.click(() => {
-        console.log("submitting")
-        let values = {}
-        for (const col of columns) {
-            console.log(col);
-            let input = $('#input' + col).val();
-            if (input == '') {
-                alert("Input field for " + col + " is empty!");
-                return;
-            }
-            if (types[col] == "INTEGER") {
-                input = parseInt(input);
-                if (isNaN(input)) {
-                    alert("Input field for " + col + " must be an integer!");
-                    return;
-                }                
-            } else if (types[col] == "REAL") {
-                input = parseFloat(input);
-                if (isNaN(input)) {
-                    alert("Input field for " + col + " must be a float!");
-                    return;
-                }
-            }
-            values[col] = input;
-        }
-        console.log(values);
-        let requestData = {
-            type: 'POST',
-            url: "/api/insert/" + activeTable,
-            data: JSON.stringify(values),
-            contentType: 'application/json',
-            success: async function () {
-                console.log("Successfully received data");
-                var request = await fetch("/api/get_all/" + activeTable);
-                const response = await request.json();
-
-                let newColumns = response["columns"];
-                let newData = response["data"];
-                $('#addButton').show();
-                fillTable($('#mainTable'), newColumns, newData);
-                return;
-            },
-            error: function () {
-                console.log("Request failed");
-                alert("Request failed");
-                return;
-            }
-        };
-        $.ajax(requestData);
+        submitRow(columns, types, null, "insert");
     })
     
     cancelButton.click(() => {
         tableRow.remove();
         $('#addButton').show();
     })
+}
+
+function editRow(columns, types, tableRow, row) {
+    tableRow.empty();
+
+    let buttonsCell = $('<th/>').appendTo(tableRow);
+    let buttonsDiv = $('<div/>', {class: "d-flex"}).appendTo(buttonsCell);
+    let submitButton = $('<button/>', {class: "btn btn-circle-sm btn-primary"}).text("Save").appendTo(buttonsDiv);
+    let cancelButton = $('<button/>', {class: "btn btn-circle-sm btn-secondary"}).text("Cancel").appendTo(buttonsDiv);
+
+    for (const col of columns) {
+        let cell = $('<th/>').appendTo(tableRow);
+        $('<input/>', {id: "input_update" + col, type: "text", class: "form-control", value: row[col]}).appendTo(cell);
+    }
+
+    submitButton.click(() => {
+        submitRow(columns, types, row, "update")
+    })
+
+    cancelButton.click(() => {
+        tableRow.empty();
+        makeRow(columns, types, tableRow, row);
+    });
+}
+
+function submitRow(columns, types, row, action) {
+    console.log("submitting")
+    let values = {}
+    for (const col of columns) {
+        console.log(col);
+        let input = $('#input_' + action + col).val();
+        console.log(input);
+        if (input == '') {
+            alert("Input field for " + col + " is empty!");
+            return;
+        }
+        if (types[col] == "INTEGER") {
+            input = parseInt(input);
+            if (isNaN(input)) {
+                alert("Input field for " + col + " must be an integer!");
+                return;
+            }                
+        } else if (types[col] == "REAL") {
+            input = parseFloat(input);
+            if (isNaN(input)) {
+                alert("Input field for " + col + " must be a float!");
+                return;
+            }
+        }
+        values[col] = input;
+    }
+    console.log(values);
+    let data;
+    if (action == "insert") {
+        data = values;
+    } else {
+        data = {"values": values, "identifiers": row};
+    }
+    let requestData = {
+        type: 'POST',
+        url: "/api/" + action + "/" + activeTable,
+        data: JSON.stringify(data),
+        contentType: 'application/json',
+        success: async function () {
+            console.log("Successfully received data");
+            var request = await fetch("/api/get_all/" + activeTable);
+            const response = await request.json();
+
+            let newColumns = response["columns"];
+            let newData = response["data"];
+            $('#addButton').show();
+            fillTable(newColumns, types, newData);
+            return;
+        },
+        error: function () {
+            console.log("Request failed");
+            alert("Request failed");
+            return;
+        }
+    };
+    $.ajax(requestData);
 }
